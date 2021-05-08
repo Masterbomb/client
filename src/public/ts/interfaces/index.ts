fe457021f9ca2e5fd9ea29a2ce225fbbd2a8628f
cSDes1gn <christian911@sympatico.ca>
2021-05-08 18:57:32 -0400

    cumulative interface exports

---
+++ src/public/ts/interfaces/index.ts
@@ -0,0 +1,13 @@
+/**
+ * index.ts
+ *
+ * Cumulative interface import
+ *
+ * @module index.ts
+ */
+
+export { Supplier } from './supplier';
+export { Project } from './project';
+export { Mf } from './manufacturer';
+export { Bom } from './bom';
+export { Part } from './part';

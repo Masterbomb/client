cffa547346295b3ebcde79cf1e99fecd1e04e4f6
cSDes1gn <christian911@sympatico.ca>
2021-05-08 15:20:14 -0400

    remove axios call from page render

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,5 @@
 import { Router } from 'express';
 import express from 'express';
-import axios from 'axios';
-import { ISuppliers } from 'api/v1/interfaces/suppliers';
 
 const pageRoutes = Router();
 
@@ -27,15 +25,7 @@ pageRoutes.get('/projects', (_req, response:express.Response) => {
 
 /* GET suppliers page */
 pageRoutes.get('/suppliers', (_req, response:express.Response) => {
-    // axios request for all suppliers
-    axios.get<ISuppliers[]>("http://localhost:3000/v1/suppliers", {
-        headers: {"Content-Type": "application/json"}
-    }).then(axios_response => {
-        // pass ejs the supplier list
-        response.render('pages/suppliers',
-            { title: 'Suppliers', suppliers: axios_response.data }
-        );
-    });
+    response.render('pages/suppliers', { title: 'Suppliers' });
 });
 
 /* GET manufacturers page */

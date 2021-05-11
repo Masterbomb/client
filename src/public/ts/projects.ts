b55e6879d141ae12b694551cf27d5e4ebc33c79e
cSDes1gn <christian911@sympatico.ca>
2021-05-10 23:21:36 -0400

    typed projects DOM backend

--- src/public/ts/projects.ts
+++ src/public/ts/projects.ts
@@ -1,14 +1,16 @@
+import $ from "jquery";
+import { Requests } from "./requests";
+import { get_selected } from "./helpers"
 import { Project } from './interfaces/project';
 
 // global suppliers state
+var projects:Project.Schema[] = [];
 var $table = $('#projectsTable').bootstrapTable();
 var $remove = $('#deleteProject');
 var $add = $('#addProject');
 var $edit = $('#editProject');
 var $post = $('#post');
 var $put = $('#put');
-var selections = [];
-var projects = [];
 
 // DOM Ready
 $(() => {
@@ -22,56 +24,35 @@ $(() => {
     $table.on('check.bs.table uncheck.bs.table ' + 'check-all.bs.table uncheck-all.bs.table',function () {
         $remove.prop('disabled', !$table.bootstrapTable('getSelections').length);
         $edit.prop('disabled', $table.bootstrapTable('getSelections').length !== 1);
-        // save your data, here just save the current page
-        selections = get_id_selection();
     });
     // once data is loaded into table hide the loading screen
-    $table.on('post-body.bs.table', function (e, args) {
+    $table.on('post-body.bs.table', function (_:JQuery.Event, _1:any) {
         $table.bootstrapTable('hideLoading');
     });
     console.log("Projects DOM Ready");
     get_state();
 });
 
-// get the ids of all selected elements
-function get_id_selection () {
-    return $.map($table.bootstrapTable('getSelections'), function (row) {
-        return row.id;
-    })
-}
-
-function loading_template() {
-    return '<div class="spinner-border text-light" role="status"><span class="sr-only">Loading...</span></div>'
-}
-
 // populate table with current state
-function get_state() {
+function get_state():void {
     $table.bootstrapTable('showLoading');
     console.log("Fetching project table state");
-    console.log(`API GET ${Project.endpoint}`);
-    $.get({
-        url: Project.endpoint 
-    }).then((data) => {
-        // update global state
-        projects = data.reverse();
-        console.log("Response: ", projects);
-        // inject html for table use load for event listener
-        // temp timeout for load wheel debug
-        $table.bootstrapTable('load', projects);
-    }).catch(() => {
-        console.error("API request failed");
-        alert("API Request Failed");
-    });
+    Requests.get<Project.Schema>(Project.endpoint).then((response) => {
+        if (response != null) {
+            projects = response.data.reverse()
+            console.log("Response Data: ", projects);
+        }
+    })
     // manually reset remove and edit options since the table selections are cleared on reload
     $remove.prop('disabled', true);
     $edit.prop('disabled', true);
 }
 
 // spawn projects form modal
-function add(event) {
+function add(event:JQuery.Event):void {
     event.preventDefault();
     // clear inputs set button
-    $('#projectForm input').each(function (index, val) {
+    $('#projectForm input').each(function (_:number, _1:HTMLElement) {
         $(this).val('');
     });
     $('#projectForm textarea').val('');
@@ -83,11 +64,12 @@ function add(event) {
     $('#formModal').modal();
 }
 
-function edit(event) {
+function edit(event:JQuery.Event):void {
     event.preventDefault();
     // inputs reflect selection
+    let id = get_selected($table)[0]
     projects.forEach(project => {
-        if (project.state === true) {
+        if (project.id === id) {
             $('#projectName').val(project.name);
             $('#projectDesc').val(project.description);
         }
@@ -99,110 +81,62 @@ function edit(event) {
     $('#formModal').modal();
 }
 
-// post new project
-function post_project(event) {
-    console.log("Validating project form");
+function post_project(event:JQuery.Event):void {
     event.preventDefault();
-    // basic form validation
-    var error_flag = false;
-    $('#projectForm input').each(function (index, val) {
-        if ($(this).val() === '') {
-            error_flag = true;
-        }
-    });
-    if (error_flag == true) {
-        console.error("Validation failed");
-        alert("Enter all required fields");
-        return false;
-    }
     // start post request
+    let name = $('#projectForm #projectName').val() as string
+    let description = $('#projectForm #projectDesc').val() as string
     const payload:Project.Schema = {
-        'name': $('#projectForm #projectName').val(),
-        'description': $('#projectForm #projectDesc').val()
+        'name': name,
+        'description': description
     };
     console.log(`API POST ${Project.endpoint} with: `, {...payload});
-    $.post({
-        data: payload,
-        url: Project.endpoint,
-        dataType:'JSON'
-    }).then(() => {
-       // clear fields
-       $('#projectForm input').val('');
-       // hide modal
-       $('#formModal').modal('toggle');
-       // rerequest get requests
-       get_state();
-    }).catch(() => {
-        console.error("API request failed");
-        alert("API Request Failed");
-    });
-}
-
-// put new project 
-function put_project(event) {
-    console.log("Validating project form");
-    event.preventDefault();
-    // basic form validation
-    var error_flag = false;
-    $('#projectForm input').each(function (index, val) {
-        if ($(this).val() === '') {
-            error_flag = true;
-        }
-    });
-    if (error_flag == true) {
-        console.error("Validation failed");
-        alert("Enter all required fields");
-        return false;
-    }
-    // here only a single id field can be selected so this getter is safe
-    // start post request
-    const payload:Project.Schema = {
-        'id': get_id_selection()[0],
-        'name': $('#projectForm #projectName').val(),
-        'description': $('#projectForm #projectDesc').val()
-    };
-    console.log(`API POST ${Project.endpoint} with: `, {...payload});
-    $.ajax({
-        type: 'PUT',
-        data: payload,
-        url: Project.endpoint,
-        dataType:'JSON',
-        success: () => {
+    Requests.post(Project.endpoint, payload).then((response) => {
+        if (response != null) {
             // clear fields
             $('#projectForm input').val('');
             // hide modal
             $('#formModal').modal('toggle');
             // rerequest get requests
             get_state();
-        },
-        error: (xhr) => {
-            console.error(`API request failed with status code: ${xhr.status}`);
-            alert("API Request Failed");
         }
     });
 }
 
-// delete one or more projects 
-function delete_project(event) {
+function put_project(event:JQuery.Event):void {
     event.preventDefault();
-    var ids = get_id_selection();
-    var promises = []
+    // here only a single id field can be selected so this getter is safe
+    // start post request
+    let id = get_selected($table)[0];
+    let name = $('#projectForm #projectName').val() as string;
+    let description = $('#projectForm #projectDesc').val() as string;
+    const payload:Project.Schema = {
+        'id': id,
+        'name': name,
+        'description': description
+    };
+    console.log(`API POST ${Project.endpoint} with: `, {...payload});
+    Requests.put(Project.endpoint, payload).then((response) => {
+        if (response != null) {
+           // clear fields
+           $('#projectForm input').val('');
+           // hide modal
+           $('#formModal').modal('toggle');
+           // rerequest get requests
+           get_state(); 
+        }
+    });
+}
+
+function delete_project(event:JQuery.Event):void {
+    event.preventDefault();
+    let ids = get_selected($table);
+    let promises:Promise<any>[] = []
     // compile promises
     ids.forEach(id => {
         let endpoint = `${Project.endpoint}${id}`;
-        console.log(`API DELETE ${endpoint}`);
         promises.push(
-            $.ajax({
-                url: endpoint,
-                type: 'DELETE',
-                dataType: 'json',
-                success: function() {},
-                error: function(response) {
-                    console.log(response);
-                    console.error("API request failed");
-                    alert("API Request Failed");
-                }
-            })
+           Requests.del(endpoint) 
         );
     })
     console.log("Promises: ", promises);

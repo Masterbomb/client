6ff1bbfcfaa8be4951e5ef17c88bbf3dc4610be0
cSDes1gn <christian911@sympatico.ca>
2021-05-12 23:07:29 -0400

    enforcing arrow syntax

--- src/public/ts/helpers/index.ts
+++ src/public/ts/helpers/index.ts
@@ -2,7 +2,7 @@ import $ from "jquery";
 
 // get the ids of all selected elements
 export function get_selected ($table:any):number[] {
-    return $.map($table.bootstrapTable('getSelections'), function (row) {
+    return $.map($table.bootstrapTable('getSelections'), (row)=> {
         return row.id;
-    })
+    });
 }

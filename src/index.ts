7b02a12cc3090702cb2c3ff401cbe3bde0940a66
cSDes1gn <christian911@sympatico.ca>
2021-05-15 02:01:33 -0400

    whitespace

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import pageRoutes from './routes/index.js';
 import dotenv from "dotenv";
 import { NextFunction, Response, Request } from 'express';
 import { fileURLToPath } from 'url';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // load environment settings

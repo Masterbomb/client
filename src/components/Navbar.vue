17dbd247341755970cf49f5e3184742bb155f2e1
cSDes1gn <christian911@sympatico.ca>
2021-10-19 13:36:00 -0400

    add navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-app-bar dense>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>Masterbom</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+<script lang="ts">
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Navbar",
+  data: (): unknown => ({
+    drawer: false,
+    group: null,
+  }),
+  computed: {
+    ...mapGetters(["authUser"]),
+  },
+};
+</script>
+<style lang="ts"></style>

3ead340f1fe59646c8ed1fad73a2f543ab482e1d
cSDes1gn <christian911@sympatico.ca>
2021-10-19 11:21:27 -0400

    fix eslint conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,53 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
-    "plugin:vue/essential",
+    "plugin:vue/recommended",
     "eslint:recommended",
     "@vue/typescript/recommended",
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint",
   ],
+
   parserOptions: {
     ecmaVersion: 2020,
   },
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/no-unused-vars": [{
-      "ignorePattern": '^_',
-    }],
-    "no-unused-vars": ["error", {
-      "argsIgnorePattern": "^_",
-      "varsIgnorePattern": "^_",
-      "args": "all",
-      "vars": "all"
-    }],
-    "vue/max-attributes-per-line": ["warning", {
-      "singleline": {
-        "max": 1,
-        "allowFirstLine": true
-      },      
-      "multiline": {
-        "max": 1,
-        "allowFirstLine": false
+    'no-console': 'on',
+    'no-debugger': 'on',
+    'vue/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        args: 'all'
       }
-    }]
+    ],
+    'vue/max-attributes-per-line': [
+      'warning',
+      {
+        singleline: {
+          max: 1,
+          allowFirstLine: true
+        },
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ],
+    'vue/no-unused-components': 'error'
   },
+
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
         jest: true,

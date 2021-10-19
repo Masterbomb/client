9a7325f633a7253ce3ed0c5c510697b6b18a6b62
cSDes1gn <christian911@sympatico.ca>
2021-10-19 13:36:20 -0400

    home component

---
+++ src/views/Console.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <navbar />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Navbar from "@/components/Navbar.vue";
+
+export default Vue.extend({
+  name: "Console",
+
+  components: {
+    Navbar,
+  },
+});
+</script>

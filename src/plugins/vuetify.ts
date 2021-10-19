1e22b32a908395337b5cb21721c3520d02376181
cSDes1gn <christian911@sympatico.ca>
2021-10-19 13:36:29 -0400

    add vuetify dark theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,4 +3,17 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: "#3f51b5",
+        secondary: "#b0bec5",
+        accent: "#8c9eff",
+        error: "#b71c1c",
+        background: "#272c2e",
+      },
+    },
+  },
+});

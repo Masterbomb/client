e21627d4fac01ef61498bde239f1d7b9799e68ac
cSDes1gn <christian911@sympatico.ca>
2021-10-21 21:58:04 -0400

    add in page routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,41 +1,51 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Console from "@/views/Console.vue";
-// import Manufacturers from "@/views/Manufacturers.vue";
+import Manufacturers from "@/views/Manufacturers.vue";
+import Parts from "@/views/Parts.vue";
+import Bom from "@/views/Bom.vue";
+import Projects from "@/views/Projects.vue";
+import Profile from "@/views/Profile.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "Console",
+    name: "/",
     component: Console,
-    // children: [
-    //   //   {
-    //   //     path: "bom",
-    //   //     name: "bom",
-    //   //     component: ,
-    //   //     props: true,
-    //   //   },
-    //   {
-    //     path: "manufacturers",
-    //     name: "manufacturers",
-    //     component: Manufacturers,
-    //     props: true,
-    //   },
-    //   //   {
-    //   //     path: "suppliers",
-    //   //     name: "suppliers",
-    //   //     component: ,
-    //   //     props: true,
-    //   //   },
-    //   //   {
-    //   //     path: "parts",
-    //   //     name: "parts",
-    //   //     component: ,
-    //   //     props: true,
-    //   //   }
-    // ],
+    children: [
+      {
+        path: "bom",
+        name: "bom",
+        component: Bom,
+        props: true,
+      },
+      {
+        path: "manufacturers",
+        name: "manufacturers",
+        component: Manufacturers,
+        props: true,
+      },
+      {
+        path: "projects",
+        name: "projects",
+        component: Projects,
+        props: true,
+      },
+      {
+        path: "parts",
+        name: "parts",
+        component: Parts,
+        props: true,
+      },
+      {
+        path: "profile",
+        name: "profile",
+        component: Profile,
+        props: true,
+      },
+    ],
   },
 ];
 

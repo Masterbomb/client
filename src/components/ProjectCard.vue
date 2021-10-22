a4cc1f69368f17a7f7f47e85559a24df778504c4
cSDes1gn <christian911@sympatico.ca>
2021-10-22 03:34:28 -0400

    designing project card view for home page

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-container class="ma-0 pa-0" justify-center align-center>
+    <v-hover v-slot="{ hover }">
+      <v-card
+        :id="'project-' + id"
+        ref="card"
+        class="
+          text-center
+          fill-height
+          rounded-tl-xl rounded-tr-xl rounded-bl-xl rounded-br-xl
+          transition-swing
+        "
+        min-height="400px"
+        max-width="1000px"
+        width="100%"
+        style="cursor: pointer"
+        color="background"
+        :class="`elevation-${hover ? 20 : 5}`"
+        :ripple="false"
+      >
+        <v-list-item one-line>
+          <v-list-item-title> {{ name }} </v-list-item-title>
+        </v-list-item>
+        <v-card-text>
+          <v-row align="center">
+            <v-col cols="6">
+              <v-list-item class="pa-0">
+                <v-list-item-content>
+                  <v-progress-circular
+                    :rotate="-90"
+                    :size="100"
+                    :width="15"
+                    :value="partProgress"
+                    :color="calcColor(partProgress)"
+                  >
+                    {{ partProgress }}
+                  </v-progress-circular>
+                  <v-list-item-title> Parts </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+            <v-col cols="6">
+              <v-list-item class="pa-0">
+                <v-list-item-content>
+                  <h1>1/2</h1>
+                  <v-list-item-title> Units </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-slider
+          v-model="time"
+          :max="6"
+          :tick-labels="labels"
+          class="mx-4"
+          ticks
+        ></v-slider>
+      </v-card>
+    </v-hover>
+  </v-container>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+@Component
+export default class ProjectCard extends Vue {
+  @Prop({ required: true }) readonly id!: number;
+  @Prop({ required: true }) readonly name!: string;
+  @Prop({ required: true }) readonly partProgress!: number;
+  @Prop({ required: true }) readonly timeline!: Array<Date>;
+
+  public calcColor(progress: number): string {
+    if (progress < 25) {
+      //statements;
+      return "red";
+    } else if (progress < 50) {
+      return "orange";
+    } else if (progress < 75) {
+      return "orange";
+    }
+    return "green";
+  }
+}
+</script>
+<style lang="scss"></style>

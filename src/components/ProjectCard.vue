0880da3990ff38bdcbaad30855b6124f58150775
cSDes1gn <christian911@sympatico.ca>
2021-10-22 23:02:29 -0400

    add sparkline for project card and align items

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,78 +1,79 @@
 <template>
-  <v-container class="pa-8 ma-4">
-    <v-hover v-slot="{ hover }">
-      <v-card
-        :id="'project-' + id"
-        class="
-          text-center
-          fill-height
-          rounded-tl-xl rounded-tr-xl rounded-bl-xl rounded-br-xl
-          transition-swing
-        "
-        max-width="1000px"
-        width="100%"
-        style="cursor: pointer"
-        color="background"
-        :class="`elevation-${hover ? 20 : 5}`"
-        :ripple="false"
-      >
-        <v-container fill-height fluid>
-          <v-row align="center" justify="center">
-            <v-col>
-              <v-list-item one-line>
-                <v-list-item-title>
-                  <h1>{{ name }}</h1>
-                </v-list-item-title>
-              </v-list-item>
-              <v-card-text>
-                <v-row align="center">
-                  <v-col cols="6">
-                    <v-list-item class="pa-0">
-                      <v-list-item-content>
-                        <v-progress-circular
-                          :rotate="-90"
-                          :size="100"
-                          :width="15"
-                          :value="(orderedParts / totalParts) * 100"
-                          :color="calcColor((orderedParts / totalParts) * 100)"
-                        >
-                          {{ orderedParts }} / {{ totalParts }}
-                        </v-progress-circular>
-                        <v-list-item-title> Parts </v-list-item-title>
-                      </v-list-item-content>
-                    </v-list-item>
-                  </v-col>
-                  <v-col cols="6">
-                    <v-list-item class="pa-0">
-                      <v-list-item-content>
-                        <v-progress-circular
-                          :rotate="-90"
-                          :size="100"
-                          :width="15"
-                          :value="partProgress"
-                          :color="calcColor(partProgress)"
-                        >
-                          {{ completedUnits }} / {{ totalUnits }}
-                        </v-progress-circular>
-                        <v-list-item-title> Units </v-list-item-title>
-                      </v-list-item-content>
-                    </v-list-item>
-                  </v-col>
-                </v-row>
-              </v-card-text>
-              <v-slider
-                v-model="time"
-                :max="6"
-                :tick-labels="labels"
-                class="mx-4"
-                ticks
-              ></v-slider>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card>
-    </v-hover>
-  </v-container>
+  <v-hover v-slot="{ hover }">
+    <v-card
+      :id="'project-' + id"
+      class="
+        text-center
+        fill-height
+        rounded-tl-xl rounded-tr-xl rounded-bl-xl rounded-br-xl
+        transition-swing
+      "
+      max-height="400px"
+      max-width="400px"
+      width="100%"
+      style="cursor: pointer"
+      color="background"
+      :class="`elevation-${hover ? 20 : 5}`"
+      :ripple="false"
+    >
+      <v-container fill-height fluid>
+        <v-row>
+          <v-col>
+            <v-list-item-title class="pa-4">
+              <h1>{{ name }}</h1>
+            </v-list-item-title>
+
+            <v-row align="center">
+              <v-col cols="6">
+                <v-list-item class="pa-0">
+                  <v-list-item-content>
+                    <v-progress-circular
+                      :rotate="-90"
+                      :size="100"
+                      :width="15"
+                      :value="(orderedParts / totalParts) * 100"
+                      :color="calcColor((orderedParts / totalParts) * 100)"
+                    >
+                      {{ orderedParts }} / {{ totalParts }}
+                    </v-progress-circular>
+                    <v-list-item-title class="subtitle">
+                      Parts
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-col>
+              <v-col cols="6">
+                <v-list-item class="pa-0">
+                  <v-list-item-content>
+                    <v-progress-circular
+                      :rotate="-90"
+                      :size="100"
+                      :width="15"
+                      :value="(completedUnits / totalUnits) * 100"
+                      :color="calcColor((completedUnits / totalUnits) * 100)"
+                    >
+                      {{ completedUnits }} / {{ totalUnits }}
+                    </v-progress-circular>
+                    <v-list-item-title class="subtitle">
+                      Units
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-col>
+            </v-row>
+            <v-sparkline
+              :fill="true"
+              :gradient="selectedGradient"
+              :padding="20"
+              :smooth="10"
+              :value="value"
+              auto-draw
+            ></v-sparkline>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-hover>
 </template>
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
@@ -87,6 +88,9 @@ export default class ProjectCard extends Vue {
   @Prop({ required: true }) readonly Progress!: number;
   @Prop({ required: true }) readonly timeline!: Array<Date>;
 
+  private selectedGradient = ["#f72047", "#ffd200", "#1feaea"];
+  private value = [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0];
+
   public calcColor(progress: number): string {
     if (progress < 25) {
       //statements;
@@ -100,4 +104,8 @@ export default class ProjectCard extends Vue {
   }
 }
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+.subtitle {
+  padding-top: 7%;
+}
+</style>

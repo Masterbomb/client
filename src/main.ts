a96799a243bf1f37507d2d85426160f27ad4442c
cSDes1gn <christian911@sympatico.ca>
2021-10-26 19:11:16 -0400

    dotenv config

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,17 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import VueLoggerPlugin from "vuejs-logger/dist/vue-logger";
+import {
+  Category,
+  CategoryServiceFactory,
+  CategoryConfiguration,
+  LogLevel,
+} from "typescript-logging";
+import dotenv from "dotenv";
+
+// load environment settings
+dotenv.config();
+
 const isProduction = process.env.NODE_ENV === "production";
 
 const options = {
@@ -26,3 +37,12 @@ new Vue({
 }).$mount("#app");
 
 Vue.use(VueLoggerPlugin, options);
+
+// Optionally change default settings, in this example set default logging to Info.
+// Without changing configuration, categories will log to Error.
+CategoryServiceFactory.setDefaultConfiguration(
+  new CategoryConfiguration(LogLevel.Debug)
+);
+
+// Create categories, they will autoregister themselves, one category without parent (root) and a child category.
+export const log = new Category("service");
